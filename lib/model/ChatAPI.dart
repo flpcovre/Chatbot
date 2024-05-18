d8a71b9f30c11b6a0579e9558fa9d8b33baf1476
flpcovre PC <filipecovre224@gmail.com>
2024-05-18 17:29:39 -0300

    Integração com a API da openai realizada, MVC criado

--- lib/model/ChatAPI.dart
+++ lib/model/ChatAPI.dart
@@ -7,7 +7,7 @@ class ChatAPI {
   final String _assistantId = 'asst_azoqVHr0SVa3oOmA9owDoySB';
   final _requestApi = const Api(headers: <String, String>{
                                 'Content-Type': 'application/json',
-                                'Authorization': 'Bearer sk-proj-pICEJlRPLEArq500MaLRT3BlbkFJLbweO7VcmKDT7PrwApp7',
+                                'Authorization': 'Bearer (Insira a chave da API OPENAI)',
                                 'OpenAI-Beta': 'assistants=v2'
                               });
 
